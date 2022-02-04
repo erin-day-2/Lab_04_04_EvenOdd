6104eb3f643988399cdc64f5c9267d78fbd8a9d1
erin-day-2 <daye4@mail.uc.edu>
2022-02-04 16:04:01 -0500

    ec work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	int numToExamine = 2;
+    int modulo = 2;
+     if (numToExamine % 2 == 0)
+        {
+            System.out.println("The number is even!");
+        }
+     else
+        {
+            System.out.println("The number is odd!");
+        }
     }
 }
